5497cb9f89849523d851fd1d64a8e3daf9e90025
Felipe <jfpastrana.uninorte.edu.co>
2023-05-09 23:31:16 -0500

    base de datos definitiva con contrasenas limitadas

--- sql/MYUN.sql
+++ sql/MYUN.sql
@@ -8,6 +8,12 @@ drop database if exists myundb;
 create database myundb;
 
 use myundb;
+/*==============================================================*/
+/* DBMS name:      MySQL 5.0                                    */
+/* Created on:     9/05/2023 11:26:27 p. m.                     */
+/*==============================================================*/
+
+
 drop table if exists EVENTO;
 
 drop table if exists GRUPO;
@@ -23,9 +29,10 @@ create table EVENTO
 (
    ID_EVENTO            int not null auto_increment,
    ID_GRUPO             int not null,
-   CORREO_USUARIO       varchar(30) not null,
+   CORREO_USUARIO       varchar(60) not null,
    NOMBRE_EVENTO        varchar(30) not null,
    FECHA_HORA_EVENTO    datetime not null,
+   HORA_EVENTO          time not null,
    APROBADO             bool not null,
    primary key (ID_EVENTO, ID_GRUPO)
 );
@@ -48,7 +55,7 @@ create table GRUPO
 create table MIEMBRO_GRUPO_
 (
    ID_GRUPO             int not null,
-   CORREO_USUARIO       varchar(30) not null,
+   CORREO_USUARIO       varchar(60) not null,
    DENTROGRUPO          bool not null,
    CREADOR_GRUPO        bool not null,
    ADMIN_GRUPO          bool not null,
@@ -60,10 +67,10 @@ create table MIEMBRO_GRUPO_
 /*==============================================================*/
 create table USUARIO
 (
-   CORREO_USUARIO       varchar(30) not null,
+   CORREO_USUARIO       varchar(60) not null,
    NOMBRE_USUARIO       varchar(30) not null,
    APELLIDO_USUARIO     varchar(30) not null,
-   CONTRASENA_USUARIO   varchar(30) not null,
+   CONTRASENA_USUARIO   varchar(20) not null,
    FECHANACIMIENTO_USUARIO date not null,
    FECHAREGISTRO_USUARIO datetime not null,
    primary key (CORREO_USUARIO)
@@ -71,6 +78,9 @@ create table USUARIO
 
 alter table EVENTO add constraint FK_ORGANIZA foreign key (ID_GRUPO, CORREO_USUARIO)
       references MIEMBRO_GRUPO_ (ID_GRUPO, CORREO_USUARIO) on delete restrict on update restrict;
+
+alter table EVENTO add constraint FK_ORGANIZA2 foreign key (ID_GRUPO)
+      references GRUPO (ID_GRUPO) on delete restrict on update restrict;
 
 alter table MIEMBRO_GRUPO_ add constraint FK_MIEMBRO_GRUPO2 foreign key (CORREO_USUARIO)
       references USUARIO (CORREO_USUARIO) on delete restrict on update restrict;
