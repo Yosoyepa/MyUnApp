0d0dbc6855f250855cfccdef167bc5f88d3d3717
MRswipez <lusanchezbe@unal.edu.co>
2023-05-09 23:31:52 -0500

    se agrega la funcion para crear el grupo

---
+++ src/Crear_grupo.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+def create_user_group(group_name):
+
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="mateus0526",
+        database="myundb"
+    )
+
+    cursor = conn.cursor()
+
+    cursor.execute("CREATE TABLE IF NOT EXISTS Grupos (id INT AUTO_INCREMENT PRIMARY KEY, group_name VARCHAR(255))")
+
+    sql = "INSERT INTO Grupos (group_name) VALUES (%s)"
+    val = (group_name,)
+    cursor.execute(sql, val)
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+
+
