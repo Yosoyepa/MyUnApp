980ee584a5226b775a9197d5dc59ed0922002db4
Yosoyepa <jandradeu@unal.edu.co>
2023-05-30 12:35:54 -0500

    Cambios en la ejecucion para consultar los mensajes del grupo

--- src/Python/controller/Controller_Chat.py
+++ src/Python/controller/Controller_Chat.py
@@ -14,15 +14,20 @@ class controller_Chat(QMainWindow):
         uic.loadUi('src/resources/interface/Pagina_Chat.ui', self)
         self.pushButton_7.clicked.connect(self.actualizar_lista_widget_grupos)
         self.lista_grupos.itemClicked.connect(self.mostrar_miembros_grupos)
+        self.lista_grupos.itemClicked.connect(self.Mostrar_chat_grupal)
     
     ##Metodo para chats grupales
 
-    def chat_grupal(self, nombre_grupo):
-        #Limpiar el contenido existente en el area del chat
-        self.widget_chat.clear()
-        #Obtener los mensajes del grupo
-        self.crd.obtener_mensajes_grupo(nombre_grupo)
-        pass
+    # def Mostrar_chat_grupal(self, nombre_grupo):
+    #     #Toma el nombre del grupo
+    #     nombre_grupo = self.lista_grupos.currentItem().text()
+    #     #Limpiar el contenido existente en el area del chat
+    #     ##Pendiente de implementar.
+    #     #Obtener los mensajes del grupo
+    #     self.crd.obtener_mensajes_grupo(nombre_grupo)
+    #     #Mostrar los mensajes en el area del chat
+    #     for mensaje in self.crd.Mensajes_grupo:
+    #         self.widget_chat.append(mensaje[0] + "a las " + mensaje[1] + "de:" + mensaje[2])
 
 
 
